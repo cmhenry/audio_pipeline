a8f125a1bc2b9ab5594ed55b6d36a9b89d006279
Colin Henry <colin.henry@vanderbilt.edu>
2025-07-30 16:34:28 -0600

    sbatch error

--- hpc/process_audio_job.sh
+++ hpc/process_audio_job.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=process_audio_%j
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=colin.henry@ipz.uzh.ch
-#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
